2b3a5f6753a77cba2025df0a32818339b5b4b4d3
Chen Yang <yangc5@mail.uc.edu>
2015-09-15 20:13:00 -0500

    made pig latin work

--- PigLatin.js
+++ PigLatin.js
@@ -1,1 +1,8 @@
 'use strict';
+var prompt = require('prompt');
+prompt.start();
+prompt.get(['name'],function(error, result) {
+    var firstLetter = result.name[0];
+    var newName = result.name.replace(firstLetter, '') + firstLetter + 'ay';
+    console.log(newName);
+});
