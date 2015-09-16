3af84954ac6f71951db135fdf73e13a8f69ddc28
Chen Yang <yangc5@mail.uc.edu>
2015-09-15 20:58:13 -0500

    pig latin completed!

--- PigLatin.js
+++ PigLatin.js
@@ -1,8 +1,25 @@
 'use strict';
 var prompt = require('prompt');
+
+//function pigLatin
+function pigLatin(name) {
+    var vowels = ['a','e','i','o','u'];
+    if (vowels.indexOf(name[0])>-1){
+        return name + 'way';
+        console.log('first name is a vowel');
+    }
+    else {
+        if (vowels.indexOf(name[1])>-1){
+            return name.replace(name[0], '') + name[0] + 'ay';
+        }
+        else{
+            return name.replace(name[0]+name[1], '') + name[0]+name[1] + 'ay';
+        }
+    }
+}
+//start prompt
 prompt.start();
+//prompt user for name, then translate the name into pig latin.
 prompt.get(['name'],function(error, result) {
-    var firstLetter = result.name[0];
-    var newName = result.name.replace(firstLetter, '') + firstLetter + 'ay';
-    console.log(newName);
+    console.log(pigLatin(result.name));
 });
