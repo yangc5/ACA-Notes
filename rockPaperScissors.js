c5c817560b103b8851cc9c6de8faee3eabe5b129
Chen Yang <yangc5@mail.uc.edu>
2015-09-20 17:17:22 -0500

    refactored the code

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,59 +4,30 @@ var prompt = require('prompt');
 prompt.start();
 
 function rockPaperScissors(hands) {
-  if(hands.contains('rock')!==-1 && hands.contains('scissors')!==-1){
+  if(hands.indexOf('rock')!==-1 && hands.indexOf('scissors')!==-1){
     return 'rock';
   }
-  if(hands.contains('paper')!==-1 && hands.contains('scissors')!==-1){
+  if(hands.indexOf('paper')!==-1 && hands.indexOf('scissors')!==-1){
     return 'scissors';
   }
-  if(hands.contains('rock')!==-1 && hands.contains('paper')!==-1){
+  if(hands.indexOf('rock')!==-1 && hands.indexOf('paper')!==-1){
     return 'paper';
   }
+  return -1;
 }
 
 prompt.get(['hand1', 'hand2'], function(error, result){
   console.log('First Hand: '+result['hand1']);
   console.log('Second Hand: '+result['hand2']);
-  //player 1 is rock
-  if(result['hand1']==='rock'){
-    if(result.hand2==='scissors'){
-      console.log('Player 1 wins');
-    }
-    else if(result.hand2==='paper'){
-      console.log('Player 2 wins');
-    }
-    else if(result.hand2==='rock'){
-      console.log("It's a tie!");
-    }
+  var hands = [result.hand1, result.hand2];
+  var winningHand = rockPaperScissors(hands);
+  if (winningHand===hands[0]) {
+    console.log("Player 1 wins");
   }
-//player 1 is paper
-  if(result.hand1==='paper'){
-    if(result.hand2==='scissors'){
-      console.log('Player 2 wins');
-    }
-    else if(result.hand2==='paper'){
-      console.log("It's a tie!");
-    }
-    else if(result.hand2==='rock'){
-      console.log("Player 1 wins");
-    }
+  else if(winningHand===hands[1]) {
+    console.log("Player 2 wins");
   }
-//player 1 is scissors
-  if(result.hand1==='scissors'){
-    if(result.hand2==='scissors'){
-      console.log("It's a tie!");
-    }
-    else if(result.hand2==='paper'){
-      console.log('Player 1 wins');
-    }
-    else if(result.hand2==='rock'){
-      console.log("Player 2 wins");
-    }
+  else if(hands[0]===hands[1]){
+    console.log("It's a tie!");
   }
-
-
-
-
-
 });
